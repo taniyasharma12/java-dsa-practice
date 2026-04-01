e7e9da60a31a54b96f2fece7b508588b42d1bf2b
taniyasharma12 <85053701+taniyasharma12@users.noreply.github.com>
2026-04-01 20:25:40 +0530

    Create DecreasingTriangle.java

---
+++ patterns/DecreasingTriangle.java
@@ -0,0 +1,34 @@
+package patterns;
+
+public class DecreasingTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int n=5;
+	
+	for(int i =1; i<=n; i++) {
+		for(int j=i; j<=n; j++) {
+			
+			System.out.print("*");
+		}
+		
+		System.out.println();
+	}
+		
+	}
+
+}
+/*
+
+*****
+****
+***
+**
+*
+
+
+
+
+
+*/
