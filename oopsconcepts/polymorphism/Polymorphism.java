154891f4a2912a181c52ef6d57bf06d6a46fe518
taniyasharma12 <85053701+taniyasharma12@users.noreply.github.com>
2026-04-01 20:57:34 +0530

    Create Polymorphism.java

---
+++ oopsconcepts/polymorphism/Polymorphism.java
@@ -0,0 +1,40 @@
+package oopsconcepts.polymorphism;
+
+public class Polymorphism {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/*
+
+Polymorphism meaning in Java refers to the ability of objects to take on many forms
+Poly + Morphism = many + behaviors
+
+Compile Time polymorphism/Static Polymorphism -> behavior is defined at compile time (decide which method will get executed)
+
+Method Overloading ->
+
+add(), add(int a , int b), add(int a, int b, int c)
+
+which method will be excuted will be decided at compile time
+
+********************************************************************************
+
+Run Time polymorphism/Dynamic Polymorphism/Dynamic Method Dispatch -> 
+
+behavior is defined at run time (decide which method will get executed)
+
+Medthod Overriding ->
+
+Class B extends A
+
+class A has method - > add(int a, int b)  Class B also has method add(int a, int b)
+
+Now which method out of two will be executed will be decided at runtime
+
+
+*/
