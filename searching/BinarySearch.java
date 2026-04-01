cf695769522509134b7f62f4ebac9a6b2379ffbd
taniyasharma12 <85053701+taniyasharma12@users.noreply.github.com>
2026-04-01 20:15:03 +0530

    Create BinarySearch.java

---
+++ searching/BinarySearch.java
@@ -0,0 +1,40 @@
+package searching;
+
+public class BinarySearch {
+
+
+    public static void main(String[] args) {
+
+        int nums[] = {1, 2, 3, 4, 5, 6, 7};
+        int target = 7;
+        System.out.println(binarySearch(nums, target));
+    }
+
+    public static int binarySearch(int nums[], int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+
+            int mid = (start + end) / 2;
+
+            if (nums[mid] == target) {
+
+                return mid;
+            }
+
+            if (nums[mid] > target) {
+
+                end = mid - 1;
+
+            } else {
+
+                start = mid + 1;
+
+            }
+        }
+
+        return 0;
+    }
+}
